559f615575565a5a9fdd9e120904001e9595c2c8
phuein <phuein@gmail.com>
2014-02-06 18:41:53 +0200

    env fix

--- server.js
+++ server.js
@@ -45,8 +45,8 @@
 // *** //
 
 //*** SERVER VARIABLES ***//
-  var ip = OPENSHIFT_DIY_IP || '127.0.0.1';
-  var port = OPENSHIFT_DIY_PORT || 8080;
+  var ip = process.env.OPENSHIFT_DIY_IP || '127.0.0.1';
+  var port = process.env.OPENSHIFT_DIY_PORT || 8080;
   
   // var keyName = 'GameTest'; // Cookie name. Defaults to 'connect.sid'.
   // var secret = '6edthsej75en43g35u563t345'; // Cookie pass.
